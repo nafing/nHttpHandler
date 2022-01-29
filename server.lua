225e7cc0829b26f4ff296f740897fcbe02d8825b
nafing <nafingone@gmail.com>
2022-01-29 12:10:45 +0100

    update

--- server.lua
+++ server.lua
@@ -8,20 +8,3 @@ SetHttpHandler(function(req, res)
     res.writeHead(200, { ["Access-Control-Allow-Origin"] = "*"} )
     res.send('event received')
 end)
-
-
---  POST METHOD via web server
-
---  axios.post('http://localhost:30120/nHttpHandler', JSON.stringify({
---      val1: 'val1',
---      val2: 'val2'
---  })).then(res => {
---      console.log(res.data);
---  });
-
---  axios.post('fivem_server_ip:fivem_server_port/script_name', JSON.stringify({
---      val1: 'val1',
---      val2: 'val2'
---  })).then(res => {
---      console.log(res.data);
---  });
